7e7e7dd385f1798af47cfe2d3d0904b8f8825785
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-01-28 01:56:41 -0300

    feat: Create class ClickableHitObject

---
+++ scenes/hit_object/clickable_hit_object.gd
@@ -0,0 +1,14 @@
+class_name ClickableHitObject
+extends HitObject
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
