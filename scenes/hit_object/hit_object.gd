6ea069c0f37410394c4e80b638741041880f7d49
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-01-28 01:51:50 -0300

    feat: Create class HitObject

---
+++ scenes/hit_object/hit_object.gd
@@ -0,0 +1,19 @@
+class_name HitObject
+extends Node
+
+
+var hit_time: float = 0.0
+var speed: float = 0.5
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
