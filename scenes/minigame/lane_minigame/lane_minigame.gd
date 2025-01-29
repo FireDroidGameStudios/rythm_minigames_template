e4d073ad2e32b79dfb439a8897963dac4eb13577
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-01-29 18:57:21 -0300

    feat: Set lane hit_ratio as hit_object target_ratio

--- scenes/minigame/lane_minigame/lane_minigame.gd
+++ scenes/minigame/lane_minigame/lane_minigame.gd
@@ -21,7 +21,7 @@ func _physics_process(_delta: float) -> void:
 
 func _on_spawn_hit_object(hit_object: HitObject, args: Dictionary = {}) -> void:
 	var lane_index: int = (hit_object as LaneHitObject).lane_index
-	var lane: Path2D = lanes.get_child(lane_index)
+	var lane: HitLane = lanes.get_child(lane_index)
 	if not lane:
 		FDCore.warning(
 			"LaneMinigame: Lane with index "
@@ -34,4 +34,5 @@ func _on_spawn_hit_object(hit_object: HitObject, args: Dictionary = {}) -> void:
 	)
 	lane_follower.speed = hit_object.speed
 	lane.add_child(lane_follower)
+	hit_object.target_ratio = lane.hit_ratio
 	lane_follower.add_child(hit_object)
