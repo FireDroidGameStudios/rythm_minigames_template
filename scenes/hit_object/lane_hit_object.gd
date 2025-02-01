9e46f0e8a0e67155098af24ed813d364b6b422bd
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-02-01 09:47:54 -0300

    feat: Implement customizable despawn method

--- scenes/hit_object/lane_hit_object.gd
+++ scenes/hit_object/lane_hit_object.gd
@@ -9,3 +9,8 @@ func _init(hit_time: float = 0.0, speed: float = 0.5, lane_index: int = 0) -> vo
 	self.hit_time = hit_time
 	self.speed = speed
 	self.lane_index = lane_index
+
+
+func _on_despawn(is_missed: bool) -> void:
+	if get_parent() is LaneFollower:
+		get_parent().queue_free()
