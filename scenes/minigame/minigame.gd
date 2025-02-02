4e2956881b3dd53b3df2f7e534fab21b8b34559e
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-02-02 19:52:41 -0300

    feat: Add type getter to minigame (single_key, multi_key and click)

--- scenes/minigame/minigame.gd
+++ scenes/minigame/minigame.gd
@@ -2,6 +2,14 @@ class_name Minigame
 extends Node2D
 
 
+enum Type {
+	UNDEFINED = -1,
+	SINGLE_KEY,
+	MULTI_KEY,
+	CLICK,
+}
+
+
 signal failed_hit
 signal success_hit(ratios: Dictionary)
 signal missed_hit(hit_object: HitObject)
@@ -28,14 +36,20 @@ func spawn_hit_object(hit_object: HitObject) -> void:
 
 func enable() -> void:
 	_is_enabled = true
+	set_visible(true)
 
 
 func disable() -> void:
 	_is_enabled = false
+	set_visible(false)
 
 
 func get_transition() -> Transition:
 	return _get_transition()
+
+
+func get_type() -> Type:
+	return _get_type()
 
 
 func _get_transition() -> Transition:
@@ -44,3 +58,11 @@ func _get_transition() -> Transition:
 
 func _on_spawn_hit_object(hit_object: HitObject) -> void:
 	add_child(hit_object)
+
+
+func _get_type() -> Type:
+	return Type.UNDEFINED
+
+
+func _get_transition_type_screen_scene() -> PackedScene:
+	return null
