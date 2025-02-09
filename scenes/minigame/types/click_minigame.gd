4f8510716a9e71803d7db6dde3279b2305bcdcfa
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-02-09 16:23:27 -0300

    feat: Implement HitObject spawn on ClickMinigame

--- scenes/minigame/types/click_minigame.gd
+++ scenes/minigame/types/click_minigame.gd
@@ -2,5 +2,13 @@ class_name ClickMinigame
 extends Minigame
 
 
+@onready var hit_objects: Node2D = get_node("HitObjects")
+
+
 func _get_minigame_type() -> Type:
 	return Type.CLICK
+
+
+func _on_spawn_hit_object(hit_object: HitObject) -> void:
+	hit_objects.add_child(hit_object)
+	hit_object.global_position = (hit_object as ClickableHitObject).spawn_position
