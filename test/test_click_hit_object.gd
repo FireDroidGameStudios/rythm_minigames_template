60abcc8dfac16ba91bf5addf83ba5497ec7ba756
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-02-09 23:21:06 -0300

    test: Create Test HitObject for test click minigame

--- test/test_click_hit_object.gd
+++ test/test_click_hit_object.gd
@@ -1,6 +1,9 @@
 extends ClickableHitObject
 
 
+@onready var ratio_visualizer: Sprite2D = get_node("RatioVisualizer")
+
+
 func _on_despawn(is_missed: bool) -> void:
 	super._on_despawn(is_missed)
 	if not timeline:
@@ -9,3 +12,7 @@ func _on_despawn(is_missed: bool) -> void:
 	if not level:
 		return
 	level.play_sound_effect(preload("res://test/bubble_pop_04.wav"))
+
+
+func _update_click_visualizer(ratio: float) -> void:
+	ratio_visualizer.scale = Vector2.ONE * (1.0 - clamp(ratio, 0.0, 1.0))
